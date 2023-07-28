1571f8069c5a13b0b820c9c1fb2cceba8b4358e8
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-27 21:16:44 -0600

    Agregar archivos de proyecto.

---
+++ Dinosaurio5834324/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace Dinosaurio5834324;
+
+public partial class MainPage : ContentPage
+{
+    /// <summary>
+    /// <Createddate>27/07/2023</Createddate>
+    /// <company>INDEL</company>
+    /// <lastmodificationdate>27/07/2023</lastmodificationdate>
+    /// <lastmodificationdescription>
+    /// PROPIEDADES DE LA IMAGEN
+    /// </lastmodificationdescription>
+    /// <lastmodifierautor>ELISEO</lastmodifierautor>
+    /// </summary>
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    /// <summary>
+    /// SE EJECUTA AL PRESIONAR EL BOTON
+    /// </summary>
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        string nombre = Convert.ToString(NDino.Text);
+        double peso = Convert.ToDouble(pesKg.Text);
+        double altura = Convert.ToDouble(altMts.Text);
+
+        dinoNom.Text = "Nombre del dinosaurio: " + nombre;
+        pesoLb.Text = "Peso en LB: " + (peso * 2.2);
+        altCm.Text = "Altura en CM: " + (altura * 100);
+    }
+}
+
---
+++ Dinosaurio5834324/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Dinosaurio5834324;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
